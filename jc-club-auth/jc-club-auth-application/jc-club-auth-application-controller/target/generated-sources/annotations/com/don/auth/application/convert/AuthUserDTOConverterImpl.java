201845b9035dcde6eeaba402322749f7acd27eb6
d <ldw151526185@gmail.com>
2025-09-21 20:33:04 +0800

    subject: 用户模块

---
+++ jc-club-auth/jc-club-auth-application/jc-club-auth-application-controller/target/generated-sources/annotations/com/don/auth/application/convert/AuthUserDTOConverterImpl.java
@@ -0,0 +1,41 @@
+package com.don.auth.application.convert;
+
+import com.don.auth.application.dto.AuthUserDTO;
+import com.don.auth.domain.entity.AuthUserBO;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-09-21T20:28:59+0800",
+    comments = "version: 1.6.2, compiler: javac, environment: Java 21 (Oracle Corporation)"
+)
+public class AuthUserDTOConverterImpl implements AuthUserDTOConverter {
+
+    @Override
+    public AuthUserBO convertDTOToBO(AuthUserDTO authUserDTO) {
+        if ( authUserDTO == null ) {
+            return null;
+        }
+
+        AuthUserBO authUserBO = new AuthUserBO();
+
+        authUserBO.setId( authUserDTO.getId() );
+        authUserBO.setUserName( authUserDTO.getUserName() );
+        authUserBO.setNickName( authUserDTO.getNickName() );
+        authUserBO.setEmail( authUserDTO.getEmail() );
+        authUserBO.setPhone( authUserDTO.getPhone() );
+        authUserBO.setPassword( authUserDTO.getPassword() );
+        authUserBO.setSex( authUserDTO.getSex() );
+        authUserBO.setAvatar( authUserDTO.getAvatar() );
+        authUserBO.setStatus( authUserDTO.getStatus() );
+        authUserBO.setIntroduce( authUserDTO.getIntroduce() );
+        authUserBO.setExtJson( authUserDTO.getExtJson() );
+        authUserBO.setCreatedBy( authUserDTO.getCreatedBy() );
+        authUserBO.setCreatedTime( authUserDTO.getCreatedTime() );
+        authUserBO.setUpdateBy( authUserDTO.getUpdateBy() );
+        authUserBO.setUpdateTime( authUserDTO.getUpdateTime() );
+        authUserBO.setIsDeleted( authUserDTO.getIsDeleted() );
+
+        return authUserBO;
+    }
+}
---
+++ jc-club-auth/jc-club-auth-domain/target/generated-sources/annotations/com/don/auth/domain/converter/AuthUserBOConverterImpl.java
@@ -0,0 +1,41 @@
+package com.don.auth.domain.converter;
+
+import com.don.auth.domain.entity.AuthUserBO;
+import com.don.auth.infra.basic.entity.AuthUser;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-09-21T20:28:58+0800",
+    comments = "version: 1.6.2, compiler: javac, environment: Java 21 (Oracle Corporation)"
+)
+public class AuthUserBOConverterImpl implements AuthUserBOConverter {
+
+    @Override
+    public AuthUser convertBOToEntity(AuthUserBO authUserBO) {
+        if ( authUserBO == null ) {
+            return null;
+        }
+
+        AuthUser authUser = new AuthUser();
+
+        authUser.setId( authUserBO.getId() );
+        authUser.setUserName( authUserBO.getUserName() );
+        authUser.setNickName( authUserBO.getNickName() );
+        authUser.setEmail( authUserBO.getEmail() );
+        authUser.setPhone( authUserBO.getPhone() );
+        authUser.setPassword( authUserBO.getPassword() );
+        authUser.setSex( authUserBO.getSex() );
+        authUser.setAvatar( authUserBO.getAvatar() );
+        authUser.setStatus( authUserBO.getStatus() );
+        authUser.setIntroduce( authUserBO.getIntroduce() );
+        authUser.setExtJson( authUserBO.getExtJson() );
+        authUser.setCreatedBy( authUserBO.getCreatedBy() );
+        authUser.setCreatedTime( authUserBO.getCreatedTime() );
+        authUser.setUpdateBy( authUserBO.getUpdateBy() );
+        authUser.setUpdateTime( authUserBO.getUpdateTime() );
+        authUser.setIsDeleted( authUserBO.getIsDeleted() );
+
+        return authUser;
+    }
+}

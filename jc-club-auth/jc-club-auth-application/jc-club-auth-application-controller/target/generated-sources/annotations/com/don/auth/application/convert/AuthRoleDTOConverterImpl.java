be4d9c9e2ab469abd5e97520f527308651c32b0b
d <ldw151526185@gmail.com>
2025-09-22 19:51:48 +0800

    subject: 角色模块

---
+++ jc-club-auth/jc-club-auth-application/jc-club-auth-application-controller/target/generated-sources/annotations/com/don/auth/application/convert/AuthRoleDTOConverterImpl.java
@@ -0,0 +1,28 @@
+package com.don.auth.application.convert;
+
+import com.don.auth.application.dto.AuthRoleDTO;
+import com.don.auth.domain.entity.AuthRoleBO;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-09-22T19:46:09+0800",
+    comments = "version: 1.6.2, compiler: javac, environment: Java 21 (Oracle Corporation)"
+)
+public class AuthRoleDTOConverterImpl implements AuthRoleDTOConverter {
+
+    @Override
+    public AuthRoleBO convertDTOToBO(AuthRoleDTO authRoleDTO) {
+        if ( authRoleDTO == null ) {
+            return null;
+        }
+
+        AuthRoleBO authRoleBO = new AuthRoleBO();
+
+        authRoleBO.setId( authRoleDTO.getId() );
+        authRoleBO.setRoleName( authRoleDTO.getRoleName() );
+        authRoleBO.setRoleKey( authRoleDTO.getRoleKey() );
+
+        return authRoleBO;
+    }
+}
---
+++ jc-club-auth/jc-club-auth-domain/target/generated-sources/annotations/com/don/auth/domain/converter/AuthRoleBOConverterImpl.java
@@ -0,0 +1,28 @@
+package com.don.auth.domain.converter;
+
+import com.don.auth.domain.entity.AuthRoleBO;
+import com.don.auth.infra.basic.entity.AuthRole;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-09-22T19:46:07+0800",
+    comments = "version: 1.6.2, compiler: javac, environment: Java 21 (Oracle Corporation)"
+)
+public class AuthRoleBOConverterImpl implements AuthRoleBOConverter {
+
+    @Override
+    public AuthRole convertBOToEntity(AuthRoleBO authUserBO) {
+        if ( authUserBO == null ) {
+            return null;
+        }
+
+        AuthRole authRole = new AuthRole();
+
+        authRole.setId( authUserBO.getId() );
+        authRole.setRoleName( authUserBO.getRoleName() );
+        authRole.setRoleKey( authUserBO.getRoleKey() );
+
+        return authRole;
+    }
+}

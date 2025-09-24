d1ecf24a1d3d1dac9daa430efc2bc3e480ef63fc
d <ldw151526185@gmail.com>
2025-09-24 17:30:34 +0800

    subject: 权限模块

---
+++ jc-club-auth/jc-club-auth-application/jc-club-auth-application-controller/target/generated-sources/annotations/com/don/auth/application/convert/AuthPermissionDTOConverterImpl.java
@@ -0,0 +1,39 @@
+package com.don.auth.application.convert;
+
+import com.don.auth.application.dto.AuthPermissionDTO;
+import com.don.auth.domain.entity.AuthPermissionBO;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-09-24T16:20:51+0800",
+    comments = "version: 1.6.2, compiler: javac, environment: Java 21 (Oracle Corporation)"
+)
+public class AuthPermissionDTOConverterImpl implements AuthPermissionDTOConverter {
+
+    @Override
+    public AuthPermissionBO convertDTOToBO(AuthPermissionDTO authPermissionDTO) {
+        if ( authPermissionDTO == null ) {
+            return null;
+        }
+
+        AuthPermissionBO authPermissionBO = new AuthPermissionBO();
+
+        authPermissionBO.setId( authPermissionDTO.getId() );
+        authPermissionBO.setName( authPermissionDTO.getName() );
+        authPermissionBO.setParentId( authPermissionDTO.getParentId() );
+        authPermissionBO.setType( authPermissionDTO.getType() );
+        authPermissionBO.setMenuUrl( authPermissionDTO.getMenuUrl() );
+        authPermissionBO.setStatus( authPermissionDTO.getStatus() );
+        authPermissionBO.setShow( authPermissionDTO.getShow() );
+        authPermissionBO.setIcon( authPermissionDTO.getIcon() );
+        authPermissionBO.setPermissionKey( authPermissionDTO.getPermissionKey() );
+        authPermissionBO.setCreatedBy( authPermissionDTO.getCreatedBy() );
+        authPermissionBO.setCreatedTime( authPermissionDTO.getCreatedTime() );
+        authPermissionBO.setUpdateBy( authPermissionDTO.getUpdateBy() );
+        authPermissionBO.setUpdateTime( authPermissionDTO.getUpdateTime() );
+        authPermissionBO.setIsDeleted( authPermissionDTO.getIsDeleted() );
+
+        return authPermissionBO;
+    }
+}
---
+++ jc-club-auth/jc-club-auth-domain/target/generated-sources/annotations/com/don/auth/domain/converter/AuthPermissionBOConverterImpl.java
@@ -0,0 +1,39 @@
+package com.don.auth.domain.converter;
+
+import com.don.auth.domain.entity.AuthPermissionBO;
+import com.don.auth.infra.basic.entity.AuthPermission;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-09-24T16:16:42+0800",
+    comments = "version: 1.6.2, compiler: javac, environment: Java 21 (Oracle Corporation)"
+)
+public class AuthPermissionBOConverterImpl implements AuthPermissionBOConverter {
+
+    @Override
+    public AuthPermission convertBOToEntity(AuthPermissionBO authPermissionBO) {
+        if ( authPermissionBO == null ) {
+            return null;
+        }
+
+        AuthPermission authPermission = new AuthPermission();
+
+        authPermission.setId( authPermissionBO.getId() );
+        authPermission.setName( authPermissionBO.getName() );
+        authPermission.setParentId( authPermissionBO.getParentId() );
+        authPermission.setType( authPermissionBO.getType() );
+        authPermission.setMenuUrl( authPermissionBO.getMenuUrl() );
+        authPermission.setStatus( authPermissionBO.getStatus() );
+        authPermission.setShow( authPermissionBO.getShow() );
+        authPermission.setIcon( authPermissionBO.getIcon() );
+        authPermission.setPermissionKey( authPermissionBO.getPermissionKey() );
+        authPermission.setCreatedBy( authPermissionBO.getCreatedBy() );
+        authPermission.setCreatedTime( authPermissionBO.getCreatedTime() );
+        authPermission.setUpdateBy( authPermissionBO.getUpdateBy() );
+        authPermission.setUpdateTime( authPermissionBO.getUpdateTime() );
+        authPermission.setIsDeleted( authPermissionBO.getIsDeleted() );
+
+        return authPermission;
+    }
+}

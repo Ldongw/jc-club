b4c7c46dc51c42982eb63693b41aad81370268a5
d <ldw151526185@gmail.com>
2025-09-24 18:02:37 +0800

    subject: 权限模块

---
+++ jc-club-auth/jc-club-auth-application/jc-club-auth-application-controller/target/generated-sources/annotations/com/don/auth/application/convert/AuthRolePermissionDTOConverterImpl.java
@@ -0,0 +1,34 @@
+package com.don.auth.application.convert;
+
+import com.don.auth.application.dto.AuthRolePermissionDTO;
+import com.don.auth.domain.entity.AuthRolePermissionBO;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-09-24T18:00:22+0800",
+    comments = "version: 1.6.2, compiler: javac, environment: Java 21 (Oracle Corporation)"
+)
+public class AuthRolePermissionDTOConverterImpl implements AuthRolePermissionDTOConverter {
+
+    @Override
+    public AuthRolePermissionBO convertDTOToBO(AuthRolePermissionDTO authRolePermissionDTO) {
+        if ( authRolePermissionDTO == null ) {
+            return null;
+        }
+
+        AuthRolePermissionBO authRolePermissionBO = new AuthRolePermissionBO();
+
+        authRolePermissionBO.setId( authRolePermissionDTO.getId() );
+        authRolePermissionBO.setRoleId( authRolePermissionDTO.getRoleId() );
+        authRolePermissionBO.setPermissionId( authRolePermissionDTO.getPermissionId() );
+        List<Long> list = authRolePermissionDTO.getPermissionIdList();
+        if ( list != null ) {
+            authRolePermissionBO.setPermissionIdList( new ArrayList<Long>( list ) );
+        }
+
+        return authRolePermissionBO;
+    }
+}
---
+++ jc-club-auth/jc-club-auth-domain/target/generated-sources/annotations/com/don/auth/domain/converter/AuthRolePermissionBOConverterImpl.java
@@ -0,0 +1,28 @@
+package com.don.auth.domain.converter;
+
+import com.don.auth.domain.entity.AuthRolePermissionBO;
+import com.don.auth.infra.basic.entity.AuthRolePermission;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-09-24T18:00:21+0800",
+    comments = "version: 1.6.2, compiler: javac, environment: Java 21 (Oracle Corporation)"
+)
+public class AuthRolePermissionBOConverterImpl implements AuthRolePermissionBOConverter {
+
+    @Override
+    public AuthRolePermission convertBOToEntity(AuthRolePermissionBO authRolePermissionBO) {
+        if ( authRolePermissionBO == null ) {
+            return null;
+        }
+
+        AuthRolePermission authRolePermission = new AuthRolePermission();
+
+        authRolePermission.setId( authRolePermissionBO.getId() );
+        authRolePermission.setRoleId( authRolePermissionBO.getRoleId() );
+        authRolePermission.setPermissionId( authRolePermissionBO.getPermissionId() );
+
+        return authRolePermission;
+    }
+}

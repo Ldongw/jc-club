a9005fc5568f284384de4eef35908811ca3addfb
d <ldw151526185@gmail.com>
2025-09-28 11:26:18 +0800

    subject: wx

--- jc-club-auth/jc-club-auth-domain/target/generated-sources/annotations/com/don/auth/domain/converter/AuthUserBOConverterImpl.java
+++ jc-club-auth/jc-club-auth-domain/target/generated-sources/annotations/com/don/auth/domain/converter/AuthUserBOConverterImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-09-24T19:27:35+0800",
+    date = "2025-09-28T11:22:04+0800",
     comments = "version: 1.6.2, compiler: javac, environment: Java 21 (Oracle Corporation)"
 )
 public class AuthUserBOConverterImpl implements AuthUserBOConverter {

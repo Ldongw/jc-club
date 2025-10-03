5db3fdf014c41d12e011738b9d3bc08dd5bd0e15
d <ldw151526185@gmail.com>
2025-10-03 12:00:03 +0800

    一些细节优化

--- jc-club-gateway/src/main/java/com/don/club/gateway/auth/SaTokenConfigure.java
+++ jc-club-gateway/src/main/java/com/don/club/gateway/auth/SaTokenConfigure.java
@@ -22,7 +22,7 @@ public class SaTokenConfigure {
             // 鉴权方法：每次访问进入 
             .setAuth(obj -> {
                 // 登录校验 -- 拦截所有路由，并排除/user/doLogin 用于开放登录 
-                SaRouter.match("/auth/**", "/auth/user/doLogin", r -> StpUtil.checkRole("normal_user"));
+                SaRouter.match("/auth/**", "/auth/user/doLogin", r -> StpUtil.checkLogin());
                 SaRouter.match("/oss/**", r -> StpUtil.checkLogin());
                 SaRouter.match("/subject/subject/**", r -> StpUtil.checkPermission("subject:add"));
                 SaRouter.match("/subject/**", r -> StpUtil.checkLogin());
